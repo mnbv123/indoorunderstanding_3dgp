026dd281a4db64649f091e322858e6f6c20ff073
mnbv123 <jwheerens@gmail.com>
2020-01-14 10:53:34 -0500

    Update anglemean.m

--- common/anglemean.m
+++ common/anglemean.m
@@ -1,4 +1,4 @@
 function m = anglemean(angles)
-n = length(angles);
-m = atan2(sum(sin(angles)) / n, sum(cos(angles)) / n);
+n = length(angles); %gets number of angles
+m = atan2(sum(sin(angles)) / n, sum(cos(angles)) / n); %returns the arctan where average cosine is avg real part and vice versa
 end
