39652ebd7d3a689c9fe5f7e0e2186d647724d928
mnbv123 <jwheerens@gmail.com>
2020-01-14 10:49:08 -0500

    Update anglediff.m

--- common/anglediff.m
+++ common/anglediff.m
@@ -1,8 +1,8 @@
 function d = anglediff(a1, a2)
-
+%computes the difference in angles
 a1 = mod(a1, 2 * pi);
 a2 = mod(a2, 2 * pi);
-
+%makes argument of angle
 d = abs(a1 - a2);
 if(d > pi)
     d = 2 * pi - d;
